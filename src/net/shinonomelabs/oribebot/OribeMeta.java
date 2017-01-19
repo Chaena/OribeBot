c7ed0080c7ce6a7ba48e2420198d79b7d8366517
Eliza Bland <eliza@shinonomelabs.net>
2017-01-19 00:36:49 +0000

    Increment minor version

--- src/net/shinonomelabs/oribebot/OribeMeta.java
+++ src/net/shinonomelabs/oribebot/OribeMeta.java
@@ -10,7 +10,7 @@ package net.shinonomelabs.oribebot;
  * @author Eliza Bland
  */
 public final class OribeMeta {
-    public static final String BOT_VERSION = null;
+    public static final String BOT_VERSION = "0.2.0";
     public static final String TWITTER_CONSUMER_KEY = null;
     public static final String TWITTER_CONSUMER_SECRET = null;
     public static final String TWITTER_ACCESS_TOKEN = null;
