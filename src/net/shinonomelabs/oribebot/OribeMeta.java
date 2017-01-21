3dc6ee926010d3d44ebcee0552ae21fdce9c51f8
Eliza Bland <eliza@shinonomelabs.net>
2017-01-21 14:13:00 +0000

    add MIT license

--- src/net/shinonomelabs/oribebot/OribeMeta.java
+++ src/net/shinonomelabs/oribebot/OribeMeta.java
@@ -1,7 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The MIT License
+ *
+ * Copyright 2017 Elizabeth Bland.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package net.shinonomelabs.oribebot;
 
@@ -11,12 +29,12 @@ package net.shinonomelabs.oribebot;
  */
 public final class OribeMeta {
     public static final String BOT_VERSION = "0.3.0";
-    public static final String TWITTER_CONSUMER_KEY = null;
-    public static final String TWITTER_CONSUMER_SECRET = null;
-    public static final String TWITTER_ACCESS_TOKEN = null;
-    public static final String TWITTER_ACCESS_TOKEN_SECRET = null;
+    public static final String TWITTER_CONSUMER_KEY = "1mqnpmXyv6dstzeXb2CW5Skrz";
+    public static final String TWITTER_CONSUMER_SECRET = "M8P3DlZAjrlnwkCQhtOQfJTPOUCfzV2JYHhqvLQq8zXrh6LCiC";
+    public static final String TWITTER_ACCESS_TOKEN = "820637205358149632-mzoQPRVfHSLXO9hiFAUpvt5wyiBZ5Aj";
+    public static final String TWITTER_ACCESS_TOKEN_SECRET = "idLnd2fh7iXtl9sjvzagnyWw2gH7eAgzEpNwnk48U6bq4";
     
-    public static final String MYSQL_URL = null;
-    public static final String MYSQL_USER = null;
-    public static final String MYSQL_PASS = null;
+    public static final String MYSQL_URL = "jdbc:mysql://shinonomelabs.net/oribebot";
+    public static final String MYSQL_USER = "oribebot";
+    public static final String MYSQL_PASS = "sonyachan!!!";
 }
